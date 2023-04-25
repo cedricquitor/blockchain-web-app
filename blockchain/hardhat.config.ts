8e224123d997c628e8d2588b722a88121fb09072
cedricquitor <cedricquitor.dev@gmail.com>
2023-04-25 09:39:18 +0800

    Added localhost and Goerli in networks

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -11,6 +11,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    goerli: {
+      url: "https://goerli.infura.io/v3/4b65da1fb2be4e45aaf60782c5a305f8",
+      accounts: ["95d37e1c6710bda12a11b749ea7f30c68b1aeaf7f49bc19e518e4e31bc2c5eb7"],
+      gasPrice: 20000000000, // 20 gwei
+    },
+  },
 };
 
 export default config;
